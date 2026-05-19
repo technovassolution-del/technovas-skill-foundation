4c0f2de354f3a06191bfb7910bac27dc3a451cc3
technovassolution-del <technovassolution@gmail.com>
2026-05-19 23:49:24 +0530

    updated

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="root",
+        user="technovasuser",
+        password="Pass",
         database="technovas_skill_foundation"
         
     )
