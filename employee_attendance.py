8f9ad8cf4292ba3f278dbf5ee46f6559df646bee
technovassolution-del <technovassolution@gmail.com>
2026-08-28 23:56:46 +0530

    commited

--- employee_attendance.py
+++ employee_attendance.py
@@ -620,10 +620,3 @@ def internal_server_error(error):
 # RUN
 # ============================================================
 
-if __name__ == "__main__":
-
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
--- employee_register.py
+++ employee_register.py
@@ -712,13 +712,3 @@ def register_employee():
 # RUN APPLICATION
 # =========================================================
 
-if __name__ == "__main__":
-
-    app.run(
-
-        host="0.0.0.0",
-
-        port=5000,
-
-        debug=True
-    )
